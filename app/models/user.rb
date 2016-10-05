b06c6912153c9b4091eff645369c490290a68e16
kaileeagray <kaileeagray@gmail.com>
2016-10-05 09:22:47 -0500

    more clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
 
