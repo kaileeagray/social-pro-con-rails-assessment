d95046d923110e013c2c4e2e52f9ec4194d6e5ff
kaileeagray <kaileeagray@gmail.com>
2016-10-04 21:47:26 -0500

    update fb log in process

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,8 +4,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in @user
+      flash[:success] = ("Facebook log in successful")
+      redirect_to root_path
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
