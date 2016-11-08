39c8a809f12df073956c9e552c91c4962e7792e9
Kailee <kaileeagray@gmail.com>
2016-11-08 15:08:45 -0600

    Removed dropdowns for now. Cleaned up unneeded scss files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @list = current_user.lists.build
-      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)
+      @feed_items = List.all.paginate(page: params[:page], :per_page => 10)
     end
   end
 
